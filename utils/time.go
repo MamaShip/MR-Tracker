c06655ab587b17655a977d5f4a6d14385ff3c452
MamaShip <youdangls@gmail.com>
2022-05-14 15:42:42 +0800

    fix(time): allow empty time object in resp

--- utils/time.go
+++ utils/time.go
@@ -3,6 +3,9 @@ package utils
 import "time"
 
 func ParseTime(time_str string) (t time.Time, err error) {
+	if time_str == "" {
+		return time.Time{}, nil
+	}
 	t, err = time.Parse(time.RFC3339Nano, time_str)
 	if err != nil {
 		return t, err
